56093551157f7d278be88e3e2f7fa516d7360553
Victor <svpanguene@gmail.com>
2020-09-10 21:37:00 +0200

    Search bar add

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,28 +1,19 @@
-import React from 'react'
-
+import React, { Component } from 'react';
 import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
-
-export default class MainScreen extends React.Component{
-
-
+export default class SearchBarExample extends Component {
     render() {
-        return(
-
+        return (
             <Container>
                 <Header searchBar rounded>
                     <Item>
                         <Icon name="ios-search" />
-                        <Input placeholder="Search" />
-                        <Icon name="ios-people" />
+                        <Input placeholder="Pesquisar" />
                     </Item>
                     <Button transparent>
-                        <Text>Search</Text>
+                        <Text>Pesquisar</Text>
                     </Button>
                 </Header>
             </Container>
-
-
-       )
+        );
     }
-
 }
