8fc4ed8dfbb49a1cd5fe7a6991557396668bbd92
Elton Vilanculos <elltonvillas10@gmail.com>
2020-09-10 22:09:47 +0200

    my screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+import { Container, Header, Item, Input, Icon, Button, Text ,Content ,ListItem ,List ,Right,Left} from 'native-base';
 
 export default class MainScreen extends React.Component{
 
@@ -19,6 +19,25 @@ export default class MainScreen extends React.Component{
                         <Text>Search</Text>
                     </Button>
                 </Header>
+
+                <Content>
+
+
+                    <List>
+
+                        <ListItem itemDivider><Text>A</Text></ListItem>
+                        <ListItem><Text>Abir</Text></ListItem>
+                        <ListItem><Text>Agora</Text></ListItem>
+                        <ListItem><Text>Armario</Text></ListItem>
+
+                        <ListItem itemDivider><Text>B</Text></ListItem>
+                        <ListItem><Text>Bacia</Text></ListItem>
+                        <ListItem><Text>Banheira</Text></ListItem>
+                        <ListItem><Text>Bazar</Text></ListItem>
+
+
+                    </List>
+                </Content>
             </Container>
 
 
